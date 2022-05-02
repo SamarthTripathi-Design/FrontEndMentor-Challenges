2656c869dad4a1d080aa88a12f22ad7363aa5e67
Samarth Tripathi <45966581+SamarthTripathi-Design@users.noreply.github.com>
2022-05-02 06:30:07 +0530

    Faq commit

---
+++ faq-accordion-card/script.js
@@ -0,0 +1,7 @@
+// const handleClick = (id) => {
+//   let question = document.getElementById(id);
+//   let answers = document.getElementsByClassName("answer");
+//   answers[id - 1].classList.add("active");
+//   answers[id - 1].classList.remove("inactive");
+//   location.reload;
+// };
