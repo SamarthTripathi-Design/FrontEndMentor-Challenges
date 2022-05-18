6149210850afc7d4a0b75d02787ec58ae7f67eca
SamarthTripathi-Design <tripathi.samarth@gmail.com>
2022-05-18 10:02:44 +0530

    Faq Change

--- faq-accordion-card/script.js
+++
@@ -1,7 +0,0 @@
-// const handleClick = (id) => {
-//   let question = document.getElementById(id);
-//   let answers = document.getElementsByClassName("answer");
-//   answers[id - 1].classList.add("active");
-//   answers[id - 1].classList.remove("inactive");
-//   location.reload;
-// };
