406c20098fe572e3bb008ccda9acbf761be55631
SamarthTripathi-Design <tripathi.samarth@gmail.com>
2022-06-16 14:09:20 +0530

    creation of the expense-chart-component

--- expenses-chart-component/script.js
+++ expenses-chart-component/script.js
@@ -1,19 +1,38 @@
-const data = fetch("./Data.json")
+const data = fetch("./data.json")
   .then((response) => response.json())
-  .then((data) => console.log(data))
+  .then((data) => {
+    console.log(data);
+    return data;
+  })
   .catch((error) => console.log(error));
 
 let chartContainer = document.querySelector(".chart-container ");
 console.log(chartContainer);
 
-// data.forEach((item) => {
-//   let chart = document.createElement("div");
-//   chart.classList.add("chart");
-//   chart.innerHTML = `${item.day}`;
-// });
+// console.log(data);
 
-data.map((item) => {
-  let chart = document.createElement("div");
-  chart.classList.add("chart");
-  chart.innerHTML = `${item.day}`;
+data.then((data) => {
+  data.forEach((item) => {
+    let chart = document.createElement("div");
+    chart.classList.add("chart");
+    const height = item.amount * 2;
+    let value = "";
+    if (item.day == "wed") {
+      value = `<div class="chart-graph active" style="--height:${height}px"></div>`;
+    } else {
+      value = `<div class="chart-graph" style="--height:${height}px"></div>`;
+    }
+    chart.innerHTML = `
+    <div class ="chart-value">${item.amount}</div>  
+    <div class="chart-wrapper">
+      
+            ${value}
+              
+      </div>
+      
+      
+      <div class="chart-title">${item.day}</div>
+      `;
+    chartContainer.appendChild(chart);
+  });
 });
