6d29d3b466f43641915fb5802d97c62c0dcfacd7
SamarthTripathi-Design <tripathi.samarth@gmail.com>
2024-10-28 07:45:44 +0530

    boxsizing chnaghes

--- interactive-comments-section/src/components/CommentsCard.jsx
+++ interactive-comments-section/src/components/CommentsCard.jsx
@@ -18,45 +18,60 @@ const CommentsCard = ({
 }) => {
   // const [state, dispatch] = useReducer(dataReducer, initialState);
 
+  const handleReply = () => {};
+
   return (
     <div
       className={replyingTo ? "comment__card commment-reply" : "comment__card "}
     >
       <div className="comment__card-section1">
         <div className="comment__card-img1-container">
-          {/* <img src="../../images/icon-plus.svg" alt="icon-plus" /> */}
+          <img src="../../public/images/icon-plus.svg" alt="icon-plus" />
         </div>
         <span className="comment__card-number">{score}</span>
         <div className="comment__card-img1-container">
-          {/* <img src="../../images/icon-minus.svg" alt="icon-minus" /> */}``
+          <img src="../../public/images/icon-minus.svg" alt="icon-minus" />
         </div>
       </div>
       <div className="comment__card-section2">
         <div className="comment__card-sectiontop">
           <div className="comment__card-avatar-container">
-            IMG
-            {/* <img src={avatar} alt="avatar" /> */}
+            <img src={avatar} alt="avatar" />
           </div>
           <span className="comment__card-avatar-name">{username}</span>
           <span className="comment__card-avatar-time">{createdAt}</span>
           {!replyingTo ? (
             <div className="comment__card-button-container">
               <div className="comment__card-button-icon">
-                {/* <img src="../../images/icon-reply.svg" alt="icon-reply" /> */}
-                <button className="comment__card-button-reply">Reply</button>
+                <img
+                  src="../../public/images/icon-reply.svg"
+                  alt="icon-reply"
+                />
+                <button
+                  className="comment__card-button-reply"
+                  onClick={handleReply}
+                >
+                  Reply
+                </button>
               </div>
             </div>
           ) : (
             <div className="comment__card-button-container">
               <div className="button1">
                 <div className="comment__card-button-icon">
-                  {/* <img src="../../images/icon-delete.svg" alt="icon-delete" /> */}
+                  <img
+                    src="../../public/images/icon-delete.svg"
+                    alt="icon-delete"
+                  />
                 </div>
                 <button className="comment__card-button-Delete">Delete</button>
               </div>
               <div className="button2">
                 <div className="comment__card-button-icon">
-                  {/* <img src="../../images/icon-edit.svg" alt="icon-edit" /> */}
+                  <img
+                    src="../../public/images/icon-edit.svg"
+                    alt="icon-edit"
+                  />
                 </div>
                 <button className="comment__card-button-Edit">Edit</button>
               </div>
--- interactive-comments-section/src/components/ReplyCard.jsx
+++ interactive-comments-section/src/components/ReplyCard.jsx
@@ -1,11 +1,11 @@
 import "./replycard.scss";
+import commentJson from "../constant/data.json";
 
 const ReplyCard = () => {
   return (
     <div className="reply__card">
       <div className="reply__card-img-container">
-        {/* <img src="" alt="" /> */}
-        IMG
+        <img src={commentJson.currentUser.image.png} alt="icon-reply" />
       </div>
       <textarea className="reply__card-textarea"></textarea>
       <button className="reply__card-button">Reply</button>
